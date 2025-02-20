031de146c2081e7575e40fece603f55496f221c8
Luis Velasco <90330736+LuisVelasc@users.noreply.github.com>
2025-02-20 14:16:35 -0600

    Add files via upload

---
+++ 8_VNS/VariablesDecision_Inicial.tcl
@@ -0,0 +1,19 @@
+# Variables de decisión de nuestro modelo. Arcilla media
+# Lista de profundidades de capas de suelo, en m
+set H {5.0 8.0 22.0 28.0 31.0 43.0 48.0};
+
+# Densidad del terreno en kg/m3
+set rho {1300 1500 1400 1600 1600 1600 1800};
+
+# Módulo a cortante en kPa
+set Gr {50000 60000 75000 65000 75000 80000 90000};
+
+# Módulo de Bulk en kPa
+set Br {190000 210000 220000 200000 250000 350000 400000};
+
+# Cohesión del terreno en kPa
+set c {25.0 30.0 35.0 25.0 30.0 45.0 50.0};
+
+# Presión de referencia en kPa
+set refPress {10.0 50.0 50.0 50.0 80.0 100.0 100.0};
+
---
+++ 8_VNS/VariablesDecision_Real.tcl
@@ -0,0 +1,19 @@
+# Variables de decisión de nuestro modelo. Arcilla media
+# Lista de profundidades de capas de suelo, en m
+set H {5.5 7.0 23.0 28.8 32.6 44.5 51.5};
+
+# Densidad del terreno en kg/m3
+set rho {1450 1570 1490 1540 1650 1520 1910};
+
+# Módulo a cortante en kPa
+set Gr {57500 63000 82000 69500 77500 83500 94700};
+
+# Módulo de Bulk en kPa
+set Br {205000 227000 235000 215000 277000 359000 415000};
+
+# Cohesión del terreno en kPa
+set c {23.2 29.0 33.5 26.1 28.9 46.5 52.3};
+
+# Presión de referencia en kPa
+set refPress {5.0 30.0 70.0 80.0 85.0 95.0 105.0};
+
